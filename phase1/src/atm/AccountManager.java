6123d21d80dec392238c8c72cf9752d83b34e660
zhaojuna <adrian.zhao@mail.utoronto.ca>
2019-02-27 14:51:04 -0500

    Completed AccountManager

--- phase1/src/atm/AccountManager.java
+++ phase1/src/atm/AccountManager.java
@@ -1,32 +1,59 @@
 package atm;
 
-import java.util.*;
+import account.Account;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+//  TODO this needs heavy testing
 public class AccountManager {
-    private final List<Class> validClassTypes;
-    private Map<Class<?>, ArrayList<Object>> accountMapByType = new HashMap<>();
+    private Map<Class, ArrayList<Object>> accountMapByType;
 
-    AccountManager(Class[] validTypes) {
-        validClassTypes = new ArrayList<>(Arrays.asList(validTypes));
-
-        for (Class klass : validClassTypes)
-            accountMapByType.put(klass, new ArrayList<>());
+    AccountManager() {
+        accountMapByType = new HashMap<>();
     }
 
-    public <T> void addAccountOfType(Class<T> klass, T account) {
-        accountMapByType.get(klass).add(account);
+    public <T> void addAccount(Class<T> klass, T account) {
+        add(klass, account);
+        Class parent = klass.getSuperclass();
+
+        while (!parent.equals(Object.class)) {
+            add(parent, account);
+            parent = parent.getSuperclass();
+        }
+
+        Class[] interfaces = klass.getInterfaces();
+
+        for (Class c : interfaces)
+            add(c, account);
+
+    }
+
+    private void add(Class klass, Object account) {
+        if (accountMapByType.containsKey(klass)) {
+            accountMapByType.get(klass).add(account);
+        } else {
+            ArrayList<Object> list = new ArrayList<>();
+            list.add(account);
+            accountMapByType.put(klass, list);
+        }
     }
 
     public <T> ArrayList<T> getAccountListOfType(Class<T> klass) {
-        if (!validClassTypes.contains(klass))
-            throw new IllegalArgumentException("Class type required is not a valid classifier!");
-
-
         ArrayList<T> result = new ArrayList<>();
 
-        for (Object obj : accountMapByType.get(klass)) {
+        for (Object obj : accountMapByType.get(klass))
             result.add(klass.cast(obj));
-        }
+
+        return result;
+    }
+
+    public ArrayList<Account> getAllAccounts() {
+        ArrayList<Account> result = new ArrayList<>();
+
+        for (Object obj : accountMapByType.get(Account.class))
+            result.add((Account) obj);
 
         return result;
     }
