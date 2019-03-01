13d27ee097b35fa82cbe233589c83cd67e7796ad
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-01 03:36:25 -0500

    implements setTime method for class FileHandler.

--- phase1/src/atm/FileHandler.java
+++ phase1/src/atm/FileHandler.java
@@ -1,5 +1,27 @@
 package atm;
-
+import java.io.*;
+import java.util.Properties;
 public class FileHandler {
-    // TODO: to be implemented
+    private Properties date;
+    private Properties userData;
+    public FileHandler(){
+        date = new Properties();
+        userData = new Properties();
+    }
+
+    /**
+     *
+     * @param time the time you want to set in string.
+     * @throws Exception Throws I/O exceptions if there is a problem.
+     */
+    public void setTime(String time) throws Exception{
+        OutputStream Time = new FileOutputStream("timeConfig.properties");
+        date.setProperty("date",time);
+        date.store(Time,null);
+
+    }
+
+
+    //TODO implement setTime(), getTime(),getAccountBalance(), setAccountBalance(), getPassword().
+
 }
