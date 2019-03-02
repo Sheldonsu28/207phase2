5216b55e91a09ea9ccc733b684f8631ad72c16ff
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-02 15:07:33 -0500

    Change the implementation of the File handler to serializable.

--- phase1/src/atm/FileHandler.java
+++ phase1/src/atm/FileHandler.java
@@ -1,25 +1,56 @@
 package atm;
 import java.io.*;
+
+/**
+ * This class is responsible for handling that need to be save to files outside.
+ */
 public class FileHandler {
 
     private FileOutputStream fileSave;
     private ObjectOutputStream output;
     private FileInputStream fileRead;
-    private ObjectOutputStream input;
+    private ObjectInputStream input;
 
-    public void FileHnadler() throws Exception{
-        this.fileSave = new FileOutputStream("Outgoing.txt");
-        this.output = new ObjectOutputStream(fileSave);
-        this.fileRead = new FileInputStream("Outgoing.txt");
-        //this.input = new ObjectInputStream(fileRead);
+    public void FileHnadler(){
+        try {
+            this.fileSave = new FileOutputStream("Outgoing.txt");
+            this.output = new ObjectOutputStream(fileSave);
+            this.fileRead = new FileInputStream("Outgoing.txt");
+            this.input = new ObjectInputStream(fileRead);
+        }catch( IOException e){
+            System.out.println("FileHandler initialization failed, operation can not be completed");
+        }
     }
 
-
-    public void saveFile(BankManager data) throws Exception{
-        output.writeObject(data);
-        output.close();
-        fileSave.close();
+    /**
+     * This method will take in a BankManager object and save it to a file.
+     * @param data A bankManager object.
+     */
+    public void saveFile(BankManager data){
+        try {
+            output.writeObject(data);
+            output.close();
+            fileSave.close();
+        }catch(IOException e){
+            System.out.println("Problem encounter when saving data, files not saved. ");
+        }
     }
 
-    //TODO: implement readFile()
+    /**
+     * This class read the information from the files outside.
+     * @return return a Bank manager object.
+     */
+    public BankManager readFile(){
+        BankManager information = null;
+        try{
+            information = (BankManager) input.readObject();
+            fileRead.close();
+            input.close();
+        }catch(IOException a){
+            System.out.println("There is a problem when reading the file, file information didi not load.");
+        }catch(ClassNotFoundException e){
+            System.out.println("The require classes is not found");
+        }
+        return information;
+    }
 }
