42d65228e3aa99f07905182d34bd7682f7012693
amandawang <amandaaaaaa.wang@mail.utoronto.ca>
2019-03-03 01:51:11 -0500

    chequing test mock1

--- phase1/test/account/ChequingAccountTest.java
+++ phase1/test/account/ChequingAccountTest.java
@@ -27,7 +27,6 @@ public class ChequingAccountTest {
         ChequingAccount chequingAccount = new ChequingAccount(time, owner, initialBalance);
         int amount = 1000;
         WithdrawTransaction register = new WithdrawTransaction(owner, chequingAccount, amount);
-
         try { chequingAccount.withdraw(amount, register);
         } catch (DebtLimitExceededException d){
             d.printStackTrace();
@@ -36,16 +35,16 @@ public class ChequingAccountTest {
         }
     }
 
-    @Test
-    public void testwithdrawal() throws WithdrawException {
-        ChequingAccount chequingAccount = new ChequingAccount(time, owner, initialBalance);
-        int amount1 = 50;
-        WithdrawTransaction register1 = new WithdrawTransaction(owner, chequingAccount, amount1);
-        chequingAccount.withdraw(amount1, register1);
-        double balance = chequingAccount.getNetBalance();
-        System.out.println(balance);
-        assertTrue(balance == -50);
-    }
+//    @Test
+//    public void testWithdrawal() throws WithdrawException {
+//        ChequingAccount chequingAccount = new ChequingAccount(time, owner, initialBalance);
+//        int amount1 = 50;
+//        WithdrawTransaction register1 = new WithdrawTransaction(owner, chequingAccount, amount1);
+//        chequingAccount.withdraw(amount1, register1);
+//        double balance = chequingAccount.getNetBalance();
+//        System.out.println(balance);
+//        assertTrue(balance == -50);
+//    }
 
 
 }
