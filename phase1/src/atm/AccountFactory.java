5403a0328077b4bfcd78512c31cda4d4abda48e9
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-03 17:55:58 -0500

    Make class AccountFactory serializable

--- phase1/src/atm/AccountFactory.java
+++ phase1/src/atm/AccountFactory.java
@@ -3,12 +3,13 @@ package atm;
 import account.Account;
 import account.Growable;
 
+import java.io.Serializable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
 import java.util.Observer;
 
-public final class AccountFactory {
+public final class AccountFactory implements Serializable {
 
     <T extends Account> boolean generateDefaultAccount(User owner, Class<T> accountType, AtmTime time) {
         T account = null;
