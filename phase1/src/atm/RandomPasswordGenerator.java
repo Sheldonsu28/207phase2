92037e75516abb502ff773de6d659758bbd6c0c2
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-09 18:33:37 -0500

    Added doc Strings for class RandomPasswordGenerator.

--- phase1/src/atm/RandomPasswordGenerator.java
+++ phase1/src/atm/RandomPasswordGenerator.java
@@ -3,11 +3,23 @@ package atm;
 import java.io.Serializable;
 import java.util.Random;
 
+/**
+ * This class generate Random passwords for account's default password when the account is been created.
+ */
 public class RandomPasswordGenerator implements Serializable {
+    //Maximum and minimum length of the password.
     private int leastDigit, mostDigit;
+    //The characters that are allowed in the password.
     private char[] allowedChars;
+    // Random number.
     private Random random;
 
+    /**
+     * The class uses the random module from java to generate a random password.
+     * @param leastDigit The minimum length of the password.
+     * @param mostDigit The maximum length of the password.
+     * @param allowedChars Characters allowed in the password.
+     */
     RandomPasswordGenerator(int leastDigit, int mostDigit, String allowedChars) {
         this.leastDigit = leastDigit;
         this.mostDigit = mostDigit;
@@ -15,6 +27,10 @@ public class RandomPasswordGenerator implements Serializable {
         random = new Random();
     }
 
+    /**
+     * Generate the password using java.util.Random module.
+     * @return The password generated.
+     */
     String generatePassword() {
         int totalDigit = random.nextInt(mostDigit - leastDigit + 1) + leastDigit;
         StringBuilder password = new StringBuilder();
