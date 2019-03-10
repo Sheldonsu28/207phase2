be09e6a51c9b85f84483383fd818363bc50a868f
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-09 23:42:56 -0500

    Added doc strings to the class IllegalDepositInfoException

--- phase1/src/transaction/IllegalDepositInfoException.java
+++ phase1/src/transaction/IllegalDepositInfoException.java
@@ -1,6 +1,12 @@
 package transaction;
 
+/**
+ * This class defines the exception that happens during interpreting the deposit file.
+ */
 public class IllegalDepositInfoException extends Exception {
+    /**
+     * The String message of the exception.
+     */
     IllegalDepositInfoException() {
         super("File deposit.txt is in wrong format or does not exist! Check README.txt for more information!");
     }
