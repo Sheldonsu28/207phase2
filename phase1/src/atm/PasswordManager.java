b369fb730f20f1c4c54c4329cfd3193fd67a6d93
zhaojuna <adrian.zhao@mail.utoronto.ca>
2019-03-09 23:35:56 -0500

    Updated password manager logic. Added invocation to password inspection in bank manager.

---
+++ phase1/src/atm/PasswordManager.java
@@ -0,0 +1,63 @@
+package atm;
+
+import java.io.Serializable;
+import java.util.Random;
+import java.util.regex.Pattern;
+
+/**
+ * This class generate Random passwords for account's default password when the account is been created.
+ */
+public class PasswordManager implements Serializable {
+    //Maximum and minimum length of the password.
+    private int leastDigit, mostDigit;
+    //The regex representation of characters that are allowed in the password.
+    private Pattern regexPattern;
+    // Random number.
+    private Random random;
+    private final char[] randomPool;
+
+    /**
+     * The class uses the random module from java to generate a random password.
+     *
+     * @param leastDigit The minimum length of the password.
+     * @param mostDigit  The maximum length of the password.
+     * @param regex      regular expression of allowed characters
+     */
+    PasswordManager(int leastDigit, int mostDigit, String regex) {
+        this.leastDigit = leastDigit;
+        this.mostDigit = mostDigit;
+        regexPattern = Pattern.compile(regex);
+        random = new Random();
+        randomPool = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+                .toCharArray();
+    }
+
+    /**
+     * Generate the password using java.util.Random module.
+     *
+     * @return The password generated.
+     */
+    String generateRandomPassword() {
+        int totalDigit = random.nextInt(mostDigit - leastDigit + 1) + leastDigit;
+        StringBuilder password = new StringBuilder();
+
+        for (int digit = 0; digit < totalDigit; digit++) {
+            String c = Character.toString(randomPool[random.nextInt(randomPool.length)]);
+            if (regexPattern.matcher(c).matches()) {
+                password.append(c);
+            }
+        }
+
+        return password.toString();
+    }
+
+    public boolean isValidPassword(String password) {
+        for (char c : password.toCharArray()) {
+            if (!regexPattern.matcher(Character.toString(c)).matches())
+                return false;
+        }
+
+        return true;
+    }
+
+}
