73d9447c872595f17c5004f0cdc48c0e223ef283
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-25 14:17:26 -0400

    Remove code smell in class Employee

--- phase2/src/atm/Employee.java
+++ phase2/src/atm/Employee.java
@@ -1,16 +1,12 @@
 package atm;
 
 import account.Account;
-import account.ChequingAccount;
-import transaction.Transaction;
+
 
 import java.io.Serializable;
 
 public class Employee extends User implements Serializable {
-    private AccountFactory accountFactory;
-    private UserDatabase Users;
-    private AtmTime commonTime;
-    private PasswordManager passwordManager;
+    private BankManager thisManager;
 
     /**
      * Employee have less access than manager, but it can also act like a normal user.
@@ -19,12 +15,9 @@ public class Employee extends User implements Serializable {
      * @param dataBase The database of the users.
      * @param standardTime ATM time.
      */
-    Employee(String username, String password, UserDatabase dataBase, AtmTime standardTime) {
+    Employee(String username, String password, UserDatabase dataBase, AtmTime standardTime,BankManager manager) {
         super(username, password);
-        this.Users = dataBase;
-        this.commonTime = standardTime;
-        this.accountFactory = new AccountFactory();
-        passwordManager = new PasswordManager(12, 24, "[0-9]|[a-z]|[A-Z]");
+        this.thisManager = manager;
     }
 
     /**
@@ -36,11 +29,7 @@ public class Employee extends User implements Serializable {
      * @return              Return if the account is created or not.
      */
     public <T extends Account> boolean createAccount(String Username, Class<T> accountType, boolean isPrimary) {
-        if (Username.equals(super.getUserName())) {
-            System.out.println("You can not add accounts to yourself.");
-            return false;
-        }
-        return accountFactory.generateDefaultAccount(Users.getUser(Username),null, accountType, commonTime, isPrimary);
+        return thisManager.createAccount(Username,accountType, isPrimary);
     }
 
     /**
@@ -51,16 +40,7 @@ public class Employee extends User implements Serializable {
      * @throws UsernameOutOfRangeException      Throw this exception if the username is too long or too short.
      */
     public String createUser(String username) throws UsernameAlreadyExistException, UsernameOutOfRangeException {
-
-        if (username.length() < User.MIN_NAME_LENGTH || username.length() > User.MAX_NAME_LENGTH)
-            throw new UsernameOutOfRangeException();
-
-        String password = passwordManager.generateRandomPassword();
-        User newUser = Users.registerNewUser(username, password);
-
-        accountFactory.generateDefaultAccount(newUser,null, ChequingAccount.class, commonTime, true);
-
-        return password;
+        return thisManager.createUser(username);
     }
 
     /**
@@ -70,13 +50,6 @@ public class Employee extends User implements Serializable {
      * @return Return true if cancellation is successful, return false if the last transaction is not empty.
      */
     public boolean cancelLastTransaction(Account targetAccount) {
-        Transaction transaction = targetAccount.getLastTransaction();
-
-        if (transaction != null && transaction.isCancellable()) {
-            transaction.cancel();
-            return true;
-        }
-
-        return false;
+        return thisManager.cancelLastTransaction(targetAccount);
     }
 }
