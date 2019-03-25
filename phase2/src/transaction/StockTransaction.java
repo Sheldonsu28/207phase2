e6dbf08e6728bcb448e5a6e90456dd1bea29c3c6
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-25 14:22:11 -0400

    Add doc Strings and finish implmenting the StockTransaction class.

--- phase2/src/transaction/StockTransaction.java
+++ phase2/src/transaction/StockTransaction.java
@@ -1,31 +1,60 @@
 package transaction;
 
+import account.WithdrawException;
 import atm.User;
+import account.StockAccount;
 
 public class StockTransaction extends Transaction{
 
+    private String stockName;
     private int stockAmount;
     private int stockPrice;
+    private StockAccount fromAccount;
 
-    public StockTransaction(User user, int Amount, int price){
+    public StockTransaction(User user, StockAccount account, int Amount, int price, String stockName){
         super(user);
         this.stockAmount = Amount;
         this.stockPrice = price;
+        this.fromAccount = account;
+        this.stockName = stockName;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean isCancellable() {
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean doPerform(){
-
+        try {
+            fromAccount.withdraw(stockAmount, stockPrice, stockName, this);
+        }catch (WithdrawException e){
+            return false;
+        }
         return true;
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString(){
+        return super.toString() +
+                String.format("User %s's Account %s\t BUY IN %s With amount %d At price %d",
+                        getFromUser(),fromAccount,stockName, stockAmount, stockPrice);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean doCancel(){
-        return true;
+        throw new IllegalStateException("Billing action is not cancellable!");
     }
 }
