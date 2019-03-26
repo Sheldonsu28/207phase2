3f830c1e092179ba357d1b10743c35f0c70bf7d7
amandawang <amandaaaaaa.wang@mail.utoronto.ca>
2019-03-26 02:30:29 -0400

    incorrect time exception

--- phase2/src/account/StockAccount.java
+++ phase2/src/account/StockAccount.java
@@ -1,5 +1,6 @@
 package account;
 
+import atm.AtmTime;
 import atm.StockQuoteGetter;
 import atm.User;
 import transaction.Transaction;
@@ -12,11 +13,16 @@ public class StockAccount extends Account{
 
     private double cash;
     private TreeMap<String, Double> stocks;
+    private String currTime;
 
     StockQuoteGetter quoteGetter = new StockQuoteGetter();
 
     StockAccount(Date time, User owner) {
         super(time, owner);
+    }
+
+    public String getTime(){
+        return currTime;
     }
 
     @Override
@@ -28,9 +34,34 @@ public class StockAccount extends Account{
         double stockQuote = quoteGetter.getQuote(stockSymbol);
     }
 
-    public void withdraw(int stockAmount, double stockQuote, String stockSymbol ,Transaction register) throws WithdrawException {
-        double moneyWithdraw = stockAmount*stockQuote;
+    public void deposit(int stockAmount, double stockPrice, String stockSymbol ,Transaction register){
+        currTime = AtmTime.FORMAT_STRING;
+
+        double moneyDeposit = stockAmount*stockPrice;
+
+        if(Integer.parseInt(currTime.substring(14,15))<9 && Integer.parseInt(currTime.substring(14,15)) > 15){
+            throw new IncorrectTimeException;
+        }
+        balance += moneyDeposit;
+
+        registerTransaction(register);
+    }
+
+    public void withdraw(int stockAmount, double stockPrice, String stockSymbol ,Transaction register) throws WithdrawException {
+        currTime = AtmTime.FORMAT_STRING;
+
+        double moneyWithdraw = stockAmount*stockPrice;
+
+        if(Integer.parseInt(currTime.substring(14,15))<9 && Integer.parseInt(currTime.substring(14,15)) > 15){
+            throw new IncorrectTimeException;
+        }
+
+        if(moneyWithdraw > balance){
+            throw new InsufficientTimeException();
+        }
+
         balance -= moneyWithdraw;
+
         registerTransaction(register);
     }
 }
