d8b34a0f5690d9d9536b825eb3450839f0f31e47
Changlin Su <sheldon.su@mail.utoronto.ca>
2019-03-27 16:38:04 -0400

    Implement Child user class

---
+++ phase2/src/atm/ChildUser.java
@@ -0,0 +1,26 @@
+package atm;
+
+import account.Account;
+
+import java.util.List;
+
+public class ChildUser extends User {
+
+    private User parentAccount;
+
+    public ChildUser(String username, String password, User parent){
+        super(username,password);
+        this.parentAccount = parent;
+    }
+
+
+    @Override
+    <T extends Account> void addAccount(T account) {
+        System.out.println("You cannot add account by yourself.");
+    }
+
+    public User getParentAccount(){
+        return this.parentAccount;
+    }
+
+}
