db9b45ffd9424b1ce04dc705fa8ed6dcc2d1b922
zhaojuna <adrian.zhao@mail.utoronto.ca>
2019-03-28 12:02:33 -0400

    GUI layout optimization.

--- phase2/src/ui/Initialization.java
+++ phase2/src/ui/Initialization.java
@@ -23,14 +23,13 @@ public class Initialization extends JDialog {
         new LoginValidation(null, LoginType.MANAGER, manager, true);
 
         container = getContentPane();
-        dateInput = new JTextField(10);
+        dateInput = new JTextField(15);
         submitButton = new JButton("Submit");
         submitButton.addActionListener(e -> {
             String dateText = dateInput.getText();
             Date result = null;
 
-            String formatString = AtmTime.FORMAT_STRING;
-            SimpleDateFormat format = new SimpleDateFormat(formatString);
+            SimpleDateFormat format = new SimpleDateFormat(AtmTime.FORMAT_STRING);
 
             try {
                 result = format.parse(dateText);
@@ -47,8 +46,9 @@ public class Initialization extends JDialog {
 
         initializeLayout();
 
-        setTitle("System Initialization");
-        setBounds(10, 10, 300, 300);
+        setTitle("System Time Initialization");
+        setBounds(100, 100, 550, 250);
+        setMinimumSize(new Dimension(550, 250));
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -60,13 +60,30 @@ public class Initialization extends JDialog {
                 }
             }
         });
+        setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
         setVisible(true);
     }
 
     private void initializeLayout() {
         container.setLayout(new GridLayout(2, 1));
-        container.add(dateInput);
-        container.add(submitButton);
+
+        JPanel inputPanel = new JPanel();
+        FlowLayout flowLayout = new FlowLayout(FlowLayout.CENTER);
+        flowLayout.setVgap(20);
+        flowLayout.setHgap(20);
+        inputPanel.setLayout(flowLayout);
+        inputPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 5, 10));
+        inputPanel.add(new JLabel(
+                String.format("Please enter system initial time in format of %s:", AtmTime.FORMAT_STRING)));
+        inputPanel.add(dateInput);
+
+        JPanel submitPanel = new JPanel();
+        submitPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        submitPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
+        submitPanel.add(submitButton);
+
+        container.add(inputPanel);
+        container.add(submitPanel);
     }
 
     BankManager getManager() {
